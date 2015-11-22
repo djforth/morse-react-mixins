2191df0ff474d5aaca0597e34c16a2a4b18ad705
Adrian Stainforth <adrian@morsedigital.com>
2015-11-22 22:26:59 +0000

    recompile

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 var css_mixins  = require("./lib/css_mixins");
 var text_mixins = require("./lib/text_mixins");
-
+var widths_mixins = require("./lib/widths_mixins");
 
 exports.css_mixins  = css_mixins;
 exports.text_mixins = text_mixins;
+exports.widths_mixins = widths_mixins
